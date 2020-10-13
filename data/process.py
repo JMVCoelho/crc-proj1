3cb201971a4f1d8311be1b864cf14d60834b4531
JMVCoelho <joao.vares.coelho@tecnico.ulisboa.pt>
2020-10-13 17:26:08 +0100

    bugfix, generating projections

--- data/process.py
+++ data/process.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import networkx as nx
 
-def process(edges_path:str, name1:str, name2:str):
+
+def process(edges_path: str, name1: str, name2: str):
     # name1 = user, name2 = business
 
     g = pd.read_csv(edges_path, delim_whitespace=True)
@@ -34,18 +35,16 @@ def process(edges_path:str, name1:str, name2:str):
     return g
 
 
-def create_projections(bipartide_path:str, nodes_path:str, name:str):
-    data = pd.read_csv(bipartide_path, delim_whitespace=True, names=['source', 'target', 'weight'])
-    B = nx.from_pandas_edgelist(data, create_using=nx.DiGraph(), edge_attr="weight")
-
+def create_projections(bipartite_path: str, nodes_path: str, name: str):
+    B = nx.algorithms.bipartite.edgelist.read_edgelist(bipartite_path, nodetype=str, data=(("weight", int),))
     data = pd.read_csv(nodes_path, delim_whitespace=True, names=['nodes', 'types'])
     nodes = data.nodes.to_list()
 
-    projection = nx.projected_graph(B, nodes)
+    projection = nx.bipartite.projected_graph(B, nodes)
 
     if len(projection) > 0:
-        nx.write_weighted_edgelist(projection, name + '.edgelist')
-        print('Wrote', name + '.edgelist')
+        nx.write_edgelist(projection, name + '.edges', data=False)
+        print('Wrote', name + '.edges')
         return projection
 
     return None
@@ -55,4 +54,3 @@ create_projections('bipartite-user-business.edges', 'users-node-list.users', 'us
 create_projections('bipartite-user-business.edges', 'business-node-list.business', 'business-projection')
 
 exit(0)
-
