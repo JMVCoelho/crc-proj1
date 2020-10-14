766492248915c950148fb3a9ea6a2bd9839265b2
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2020-10-14 12:41:16 +0100

    fixed bug in create_plot refactored code

--- analysis.py
+++ analysis.py
@@ -11,41 +11,44 @@ import operator
 register_matplotlib_converters()
 
 
-def create_plot(path_to_save: str, title: str, xlabel: str, xdata: list, ylabel: str, ydata: list, yticks: list = [],
-                also_log_scale: bool = False, log_yticks: list = [], powerlaw_xmin=None):
+def create_plot(path_to_save: str, title: str, xlabel: str, xdata: list, ylabel: str, ydata: list, yticks: list = None,
+                also_log_scale: bool = False, log_yticks: list = None, powerlaw_xmin=None, powerlaw_xmax=None):
     fig = plt.figure()
     fig.set_size_inches(14.0, 14.0)
 
     # Plot in linear scale
     ax = plt.subplot(2 if also_log_scale else 1, 1, 1)
     plt.scatter(xdata, ydata)
-    plt.title(title + '(linear scale)' if also_log_scale else '')
+    plt.title(title + ' (linear scale)' if also_log_scale else '')
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    ax.set_yticks(yticks)
+    if yticks:
+        ax.set_ylim(ymin=yticks[0], ymax=yticks[len(yticks) - 1])
+        ax.set_yticks(yticks)
     plt.grid()
 
     # Plot in log scale
     if also_log_scale:
         ax = plt.subplot(2, 1, 2)
         plt.scatter(xdata, ydata)
-        plt.title(title + '(log scale)')
+        plt.title(title + ' (log scale)')
         plt.xlabel('log(' + xlabel + ')')
         plt.ylabel('log(' + ylabel + ')')
         ax.set_xscale('log')
-        ax.set_ylim(ymin=0.0001)
         ax.set_yscale('log')
-        ax.set_yticks(log_yticks)
+        if log_yticks:
+            ax.set_ylim(ymin=log_yticks[0], ymax=log_yticks[len(log_yticks)-1])
+            ax.set_yticks(log_yticks)
         ax.get_yaxis().set_major_formatter(mpl.ticker.ScalarFormatter())
         plt.grid()
 
     # Plot power law
-    if powerlaw_xmin:
-        fit = powerlaw.Fit(xdata + 1, xmin=powerlaw_xmin, discrete=True)
+    if powerlaw_xmin and powerlaw_xmax:
+        fit = powerlaw.Fit(xdata + 1, xmin=powerlaw_xmin, xmax=powerlaw_xmax, discrete=True)
     else:
         fit = powerlaw.Fit(xdata + 1, discrete=True)
 
-    fit.power_law.plot_pdf(color='r', linestyle='--', label='fit ccdf')
+    fit.power_law.plot_pdf(color='r', linestyle='--', label='fit pdf')
 
     fig.savefig(path_to_save)
 
@@ -83,41 +86,43 @@ if weighted:
 
 # 3. Degree Centrality
 degree_centrality       = nx.degree_centrality(g)
-if directed:
-    in_degree_centrality    = nx.in_degree_centrality(g)
-    out_degree_centrality   = nx.out_degree_centrality(g)
-
 stats['Degree']      = [v for k, v in degree_centrality.items()]
+functions_todo = ['Degree']
+
 if directed:
+    in_degree_centrality = nx.in_degree_centrality(g)
+    out_degree_centrality = nx.out_degree_centrality(g)
     stats['In-Degree']   = [v for k, v in in_degree_centrality.items()]
     stats['Out-Degree']  = [v for k, v in out_degree_centrality.items()]
 
-# un-normalize
-stats['Degree'] = round(stats['Degree'] * (n_nodes-1))
-if directed:
-    stats['In-Degree'] = round(stats['In-Degree']  * (n_nodes-1))
-    stats['Out-Degree'] = round(stats['Out-Degree'] * (n_nodes-1))
+    functions_todo = ['Degree', 'In-Degree', 'Out-Degree']
 
-# Top 10 nodes with highest degree
-print(stats.sort_values(by='Degree', ascending=False).head(10))
-if directed:
-    print(stats.sort_values(by='In-Degree', ascending=False).head(10))
-    print(stats.sort_values(by='Out-Degree', ascending=False).head(10))
+for function in functions_todo:
+    # un-normalize
+    stats[function] = round(stats[function] * (n_nodes-1))
+    # top 10 nodes with highest degree
+    print(stats.sort_values(by=function, ascending=False).head(10))
 
-# Degree distribution
-distribution = stats.groupby(['Degree']).size().reset_index(name='Frequency')
-sum = distribution['Degree'].sum()
-distribution['Probability'] = distribution['Degree'] / sum
-distribution.head(10)
+    # Degree distribution
+    distribution = stats.groupby([function]).size().reset_index(name='Frequency')
+    sum = distribution['Frequency'].sum()
+    distribution['Probability'] = distribution['Frequency'] / sum
+    distribution.head(10)
 
-alpha = create_plot("plots/degree_distribution.pdf", "Degree distribution",
-                    "Degree", distribution['Degree'],
-                    "Probability", distribution['Probability'],
-                    also_log_scale=True, log_yticks=[1,1e-01, 1e-02, 1e-03, 1e-04],powerlaw_xmin=1.7)
-plt.show()
-print('Degree distribution alpha= ', alpha)
+    alpha = create_plot("plots/degree_distribution.pdf", function + " distribution",
+                        function, distribution[function],
+                        "Probability", distribution['Probability'],
+                        yticks = [0, 0.001, 0.002, 0.003, 0.007],
+                        also_log_scale = True, log_yticks = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
+                        powerlaw_xmin=1e1, powerlaw_xmax=1e4)
+    plt.show()
+    print(function + ' distribution alpha= ', alpha)
 
 
+# 3. Weighted Degree Centrality
+if weighted:
+    # TODO
+    passK
 
 
 
