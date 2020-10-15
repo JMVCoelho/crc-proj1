345b465be7ff1a23f16c76dd296c3f241dde7c26
JMVCoelho <joao.vares.coelho@tecnico.ulisboa.pt>
2020-10-15 22:30:14 +0100

    changed apl

--- metrics/AveragePathLength.py
+++ metrics/AveragePathLength.py
@@ -12,8 +12,17 @@ class AveragePathLength(Metric):
     def compute(self, stats, name, pr=True):
 
         if not os.path.exists('pickle/' + name + '_average_path_length.pickle'):
-            average_shortest_path_length = nx.average_shortest_path_length(self.graph,
-                                                                           weight=self.edge_attribute_for_weight)
+            if nx.is_connected(self.graph):
+                average_shortest_path_length = nx.average_shortest_path_length(self.graph,
+                                                                               weight=self.edge_attribute_for_weight)
+            #else:
+                #pls = []
+                #for sub_graph in [self.graph.subgraph(c).copy() for c in nx.connected_components(self.graph)]:
+                    #aspl = nx.average_shortest_path_length(sub_graph)
+                    #print(aspl)
+                    #pls.append(aspl)
+                #average_shortest_path_length = sum(pls)/len(pls)
+
 
             with open('pickle/' + name + '_average_path_length.pickle', 'wb') as output:
                 pickle.dump(average_shortest_path_length, output, pickle.HIGHEST_PROTOCOL)
