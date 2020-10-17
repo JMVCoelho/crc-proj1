f87681d964f0ca07a09f7584eb133de251def483
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2020-10-17 16:49:18 +0100

    fix bug in communities and more functions

--- metrics/Communities.py
+++ metrics/Communities.py
@@ -1,6 +1,5 @@
 from metrics.Metric import Metric
 
-import pandas as pd
 import networkx as nx
 import pickle
 import os
@@ -36,35 +35,59 @@ class Communities(Metric):
             if pr:
                 print("Found", len(communities), "communities")
 
-        if not os.path.exists(name + '_communities.csv'):
-            data = pd.DataFrame()
+        if not os.path.exists(name + '_sorted_communities.pickle'):
+            # undo frozen sets and do community names
+            sorted_communities = dict()
             for i in range(0, len(communities)):
-                data.insert(i, "Community " + str(i), communities[i])
-                stats.to_csv(name + '_communities.csv', sep=' ')
-        else:
-            data = pd.read_csv(name + '_communities.csv')
+                sorted_communities["Community " + str(i + 1)] = list(communities[i])
 
-        self.data = data
-        return communities
+            # save
+            with open(name + '_sorted_communities.pickle', 'wb') as output:
+                pickle.dump(sorted_communities, output, pickle.HIGHEST_PROTOCOL)
+        else:
+            with open(name + '_sorted_communities.pickle', 'rb') as dc:
+                sorted_communities = pickle.load(dc)
+
+        self.communities = sorted_communities
+        return sorted_communities
 
     def find_node_community(self, node, stats, name, pr=True):
-
-        if self.data is not None:
-            pass
-
-        elif os.path.exists(name + '_communities.csv'):
-            self.data = pd.read_csv(name + '_communities.csv')
-
+        if os.path.exists(name + '_sorted_communities.pickle'):
+            with open(name + '_sorted_communities.pickle', 'rb') as dc:
+                self.communities = pickle.load(dc)
         else:
             self.compute(self, stats, name, pr)
 
         res = []
-        communities = self.data.columns
-        for com in communities:
-            if len(self.data.where(self.data[com] == node)) > 0:
-                res.append(com)
+
+        community_names = list(self.communities.keys())
+        community_users = list(self.communities.values())
+
+        for community in community_users:
+            if node in community:
+                res.append(community_names[community_users.index(community)])
 
         if pr:
             print("Node", node, "is in the communities:", res)
 
         return res
+
+    def community_sizes(self, stats, name, top=10, pr=True):
+        if os.path.exists(name + '_sorted_communities.pickle'):
+            with open(name + '_sorted_communities.pickle', 'rb') as dc:
+                self.communities = pickle.load(dc)
+        else:
+            self.compute(self, stats, name, pr)
+
+        community_names = list(self.communities.keys())
+
+        if isinstance(top, str) and top == "all":
+            print("\n\nAll Communities:")
+            for name in community_names:
+                print(name, len(self.communities[name]))
+
+        if isinstance(top, int):
+            print("\n\nBigger Communities:")
+            for i in range(0, top):
+                name = community_names[i]
+                print(name, len(self.communities[name]))
